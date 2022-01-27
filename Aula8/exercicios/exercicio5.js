bfe5cfa6621a1c3902bec93fb2828e68265bcfea
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-01-26 23:07:43 -0300

    Aula 8 exercicio5

--- Aula8/exercicios/exercicio5.js
+++ Aula8/exercicios/exercicio5.js
@@ -4,11 +4,11 @@ Assim, a fórmula a seguir define a sequência:
 Fn = Fn - 1 + Fn - 2
  */
 
-let fn = 1;
-let j = 1;
-let i = 0;
+let fn = 0;
+let j = 0;
+let i = 1;
 
-for (let k = 1; k < 11; k++) {
+for (let k = 0; k < 10; k++) {
   console.log(fn);
   fn = i + j;
   i = j;
