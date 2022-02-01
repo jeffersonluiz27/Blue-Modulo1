9521de91daeff4025d086fac07ff754a9030b2a5
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-01-31 22:01:56 -0300

    Aula9

--- Aula9/exercicios/exercicio1.js
+++ Aula9/exercicios/exercicio1.js
@@ -3,3 +3,13 @@ Faça um programa que peça 2 números e crie um array contando todos os
 números os números sequenciais do primeiro até o segundo, independente 
 de ser maior ou menor.
  */
+
+num1 = 2;
+num2 = 9;
+array = [];
+
+for (let i = num1; i <= num2; i++) {
+  array.push(i);
+}
+
+console.log(array);
