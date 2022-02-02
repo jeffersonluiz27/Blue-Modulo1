dea46fda4adbc325600f4a485b50ff47c2fc0f65
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-02-01 21:14:22 -0300

    Aula10

--- Aula10/exercicios/exercicio1.js
+++ Aula10/exercicios/exercicio1.js
@@ -1,3 +1,22 @@
 /**
 Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.
  */
+
+const prompt = require('prompt-sync')();
+
+const num = +prompt(`Digite um numero: `);
+let primo = true;
+
+for (let i = 2; i < num; i++) {
+  if (num % i == 0) {
+    primo = false;
+    break;
+  }
+  primo = true;
+}
+
+if (primo) {
+  console.log(`${num} é Primo!`);
+} else {
+  console.log(`${num} não é Primo!`);
+}
