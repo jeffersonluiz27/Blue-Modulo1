37176f8fd315b58624cf61a39b97749ab747eb33
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-02-02 20:16:52 -0300

    Aula11

--- Aula11/arrowfunc.js
+++ Aula11/arrowfunc.js
@@ -1,9 +1,9 @@
-// const somar = (a, b) => {
-//   const soma = a + b;
-//   return soma;
-// };
+const somar1 = (a, b) => {
+  const soma = a + b;
+  return soma;
+};
 
-// console.log(somar(15, 20));
+console.log(somar1(15, 20));
 
 const somar = (a, b) => a + b;
 
