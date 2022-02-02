ad52d91ddaffa7b5555b1e6692be69ab4d52793b
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-02-01 22:58:12 -0300

    Aula12

--- Aula12/exercicios/exercicio5.js
+++ Aula12/exercicios/exercicio5.js
@@ -4,5 +4,14 @@ Crie uma função para inverter um array passado pelo usuário.
 
 const prompt = require('prompt-sync')();
 
-const array = [];
+const array = [1, 2, 3, 4, 5, 6];
 
+function inverte(array) {
+  let troca = [];
+  for (let i = array.length; i > 0; i--) {
+    troca.push(array[i - 1]);
+  }
+  return troca;
+}
+
+console.log(inverte(array));
--- Aula12/exercicios/exercicioExtra2.js
+++ Aula12/exercicios/exercicioExtra2.js
@@ -1,3 +1,19 @@
 /**
 Criar uma função que junte dois arrays e retorno o resultado como um novo array
  */
+
+const array = [];
+function juntarArrays(array1, array2) {
+  for (let a of array1) {
+    array.push(a);
+  }
+  for (let a of array2) {
+    array.push(a);
+  }
+  return array;
+}
+
+const array1 = [1, 3, 5];
+const array2 = [2, 4, 6];
+
+console.log(juntarArrays(array1, array2));
