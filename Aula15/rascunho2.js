805628db5dc1e87125a12860c1b10a427ecbaa18
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-02-07 21:54:37 -0300

    rascunho projeto final

--- Aula15/rascunho2.js
+++ Aula15/rascunho2.js
@@ -1,37 +1,32 @@
 const prompt = require('prompt-sync')();
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 3; ) {
   var opcao = prompt(
     'Qual é a sua opcao pra essa rodada? Digite Azul, Vermelha, ou Verde: '
   );
   var resposta = Math.floor(Math.random() * 3 + 1);
   console.log(resposta);
+
   if (opcao.trim().toLowerCase() == 'azul') {
-    console.log('Você escolheu a bebida azul.');
-    if (resposta == 1) {
-      console.log('Você perdeu uma vida.');
-    } else if (resposta == 2) {
-      console.log('Você perdeu meia vida.');
-    } else {
-      console.log('Escapou! Não perdeu vida.');
-    }
+    bebidaEscolhida(opcao, resposta);
   } else if (opcao.trim().toLowerCase() == 'vermelha') {
-    console.log('Você escolheu a bebida vermelha. ');
-    if (resposta == 1) {
-      console.log('Você perdeu uma vida.');
-    } else if (resposta == 2) {
-      console.log('Você perdeu meia vida.');
-    } else {
-      console.log('Escapou! Não perdeu vida.');
-    }
+    bebidaEscolhida(opcao, resposta);
   } else if (opcao.trim().toLowerCase() == 'verde') {
-    console.log('Você escolheu a bebida verde.');
-    if (resposta == 1) {
-      console.log('Você perdeu uma vida.');
-    } else if (resposta == 2) {
-      console.log('Você perdeu meia vida.');
-    } else {
-      console.log('Escapou! Não perdeu vida.');
-    }
+    bebidaEscolhida(opcao, resposta);
+  } else {
+    console.log('Opção inválida!');
+    continue;
+  }
+  i++;
+}
+
+function bebidaEscolhida(opcao, resposta) {
+  console.log(`Você escolheu a bebida ${opcao}. `);
+  if (resposta == 1) {
+    console.log('Você perdeu uma vida.');
+  } else if (resposta == 2) {
+    console.log('Você perdeu meia vida.');
+  } else {
+    console.log('Escapou! Não perdeu vida.');
   }
 }
