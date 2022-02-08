abaae9725c178b07e3ded140fcc89d39178a1ca6
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-02-08 20:28:33 -0300

    Reconstruindo

--- Aula15/rascunho3.js
+++ Aula15/rascunho3.js
@@ -1,48 +1,284 @@
-const prompt = require('prompt-sync')();
+/*
+var prompt = require("prompt-sync")();
 
-let roundCount = 1;
+console.log("Bem-vindo ao OuterWorld!");
+const nome = prompt("Qual o nome do seu guerreiro? ");
+console.log(`Introdução ${nome} `);
 
-while (true) {
-  let playerDmgRandom = getRandomIntInclusive(playerDmgMin, playerDmgMax);
-  let opponentDmgRandom = getRandomIntInclusive(opponentDmgMin, opponentDmgMax);
+var danoRecebido = 0;
 
-  let playerDmg = playerDmgRandom;
-  let opponentDmg = opponentDmgRandom;
+const sujeitoPersonagem = {
+  nome: nome,
+  vida: 5,
+  stamina: 10,
+  itens: [],
+  porrada: function () {
+      // caso ache melhor um retorno com mais casas decimais, basta aumentar argumento de toFixed
+    const dano =
+      0.3 * this.vida + 0.5 * parseFloat((this.stamina * Math.random()).toFixed(1)) + 0.2 * this.itens;
+    return dano;
+  },
+};
+console.log(sujeitoPersonagem.nome);
 
-  playerDefense = playerDefense - opponentDmg;
-  opponentDefense = opponentDefense - playerDmg;
+//a função porrada retorna o valor de dano. Nao é possivel invocar a variável dano
 
-  console.log(`\n\tROUND ${roundCount}
-  
-\tYou attack your opponent's argument for ${playerDmg} damage.
-\tYour opponent attacks your argument for ${opponentDmg} damage.
+console.log(sujeitoPersonagem.porrada());
 
-\tYour defense:    ${playerDefense}
-\tYour opponent's: ${opponentDefense}\n`);
+// Adicionando nova skill
+sujeitoPersonagem.bolaDeFogo = function bolaDeFogo() {
+    // caso ache melhor um retorno com mais casas decimais, basta aumentar argumento de toFixed
+  const dano =
+    0.7 * this.vida + 0.5 * parseFloat((this.stamina * Math.random()).toFixed(1)) + 0.2 * this.itens;
+  return dano;
+},
 
-  roundCount++;
 
-  prompt(`    Press ENTER for next round `);
+console.log(sujeitoPersonagem);
+console.log(`Dano bola de fogo= ${sujeitoPersonagem.bolaDeFogo()}`);
 
-  if (playerDefense <= 0 || opponentDefense <= 0) {
-    console.log(`\n    Oh, it looks like that was the last round...`);
-    prompt(`    Press ENTER for results `);
+
+
+//----------------------------------------------------------------//
+
+
+
+//Reino 1 : Floresta Tropical / condado/ 
+console.log(`Você chegou na Adega do Gnomo! Aqui muitos guerreiros vem para descansar e tomar uns drinks.
+Papo vai, Papo vem e você foi desafiado para uma roleta russa com os drinks de um mago poderoso do local. 
+São 3 rodadas de shots e duas das três bebidas não fazem bem para sua saude. Uma tirará meio ponto de vida
+e a outra tirará um ponto. Espero que você escolha a certa todas as vezes, grande Guerreiro!`);
+
+console.log(`Você chegou na Adega do Gnomo! Aqui muitos guerreiros vem para descansar e tomar uns drinks.
+Papo vai, Papo vem e você foi desafiado para uma roleta russa com os drinks de um mago poderoso do local. 
+São 3 rodadas de shots e duas das três bebidas não fazem bem para sua saude. Uma tirará meio ponto de vida
+e a outra tirará um ponto. Espero que você escolha a certa todas as vezes, grande Guerreiro!`);
+
+// * Colheita com o Elfo Armador 
+// switch com 3 cases, cada um com mais duas opcoes por case que levam a itens
+// armadura 3 /escudo 2
+// foice 2 / espada 3
+// dinheiro/xxxxx
+
+
+var caminho = prompt(`Que caminho você gostaria de iniciar? Digite Bosque, Ferreiro ou Exercito`);
+
+while (caminho != `bosque` && caminho != `ferreiro` && caminho != `exercito`);
+switch (caminho) {
+  case "bosque":
+    console.log("Voce optou pelo bosque. Daqui vc tem duas opcoes de itens.");
+    var itembosque = prompt(`Quer qual das duas opcoes, que leva a pocao ou ao xxx?`);
+    if(itembosque.toLowerCase == "pocao"){
+      console.log("voce pegou a pocao de item");
+      sujeitoPersonagem.itens.pop(pocao)
+    }else {
+      console.log("voce pegou o dinheiro de item");
+      sujeitoPersonagem.itens.pop(dinheiro)
+    }
     break;
+  case "ferreiro":
+    console.log("Voce optou pelo Ferreiro. Não é uma caminhada longa, ele faz muitos adornos as elites e não pode se distanciar muito do palácio do Rei. Daqui vc tem duas opcoes..");
+    var itemferreiro = prompt(`Quer qual das duas opcoes, Armadura ou Escudo?`);
+    if(itemferreiro.toLowerCase == "armadura"){
+      console.log("voce pegou a armadura de item");
+      sujeitoPersonagem.itens.pop(armadura)
+    }else {
+      console.log("voce pegou o escudo de item");
+      sujeitoPersonagem.itens.pop(escudo)
+    }
+    break;
+  case "exercito":
+    console.log("Voce optou ir visitar a artilharia pesada. Você considera que é bom estar armado para sua jornada. Bem pensado, afinal, quais desafios você pode encontrar pelo caminho!? E como transpo-los sem nada na mão!?");
+    var itemexercito = prompt(`Você chega e é recepcionado pelo Sargeto Orveu, ele é o responsável pelas armas de combate e já aguardava a sua chegada. Orveu tem uma presença dominante e não é de muitas palavras. Pergunta rapidamente qual será a sua jornada, o que possivelmente você irá enfrentar e pega dos itens a mão. Uma espada e uma foice. Quer qual das duas opcoes, digite foice ou espada: `);
+    if(itemexercito.toLowerCase == "espada"){
+      console.log("voce pegou a espada de item");
+      sujeitoPersonagem.itens.pop(espada)
+    }else {
+      console.log("voce pegou a foice de item");
+      sujeitoPersonagem.itens.pop(foice)
+    }
+    break;
+  default:
+    console.log("Desculpe, estamos sem nenhuma " + expr + ".");
+}
+
+console.log("Tem algo mais que você gostaria de levar?");
+
+
+console.log(`Você chegou na Adega do Gnomo! Aqui muitos guerreiros vem para descansar e tomar uns drinks.
+Papo vai, Papo vem e você foi desafiado para uma roleta russa com os drinks de um mago poderoso do local. 
+São 3 rodadas de shots e duas das três bebidas não fazem bem para sua saude. Uma tirará meio ponto de vida
+e a outra tirará um ponto. Espero que você escolha a certa todas as vezes, grande Guerreiro!`);
+
+
+for (let i = 0; i < 3; ) {
+  var opcao = prompt(
+    'Qual é a sua opcao pra essa rodada? Digite Azul, Vermelha, ou Verde: '
+  );
+  var resposta = Math.floor(Math.random() * 3 + 1);
+  console.log(resposta);
+
+  if (opcao.trim().toLowerCase() == 'azul') {
+    bebidaEscolhida(opcao, resposta);
+  } else if (opcao.trim().toLowerCase() == 'vermelha') {
+    bebidaEscolhida(opcao, resposta);
+  } else if (opcao.trim().toLowerCase() == 'verde') {
+    bebidaEscolhida(opcao, resposta);
+  } else {
+    console.log('Opção inválida! Digite a palavra!\n');
+    continue;
+  }
+  i++;
+}
+
+function bebidaEscolhida(opcao, resposta) {
+  console.log(`\nVocê escolheu a bebida ${opcao}. `);
+  if (resposta == 1) {
+    console.log('Você perdeu uma vida.\n');
+  } else if (resposta == 2) {
+    console.log('Você perdeu meia vida.\n');
+  } else {
+    console.log('Escapou! Não perdeu vida.\n');
   }
 }
 
-console.clear();
+         
+console.log(`Luta chefão: Gigante da Colina -  vida - dinheiro/itens Você chegou na Adega do Gnomo! Aqui muitos guerreiros vem para descansar e tomar uns drinks.
+Papo vai, Papo vem e você foi desafiado para uma roleta russa com os drinks de um mago poderoso do local. 
+São 3 rodadas de shots e duas das três bebidas não fazem bem para sua saude. Uma tirará meio ponto de vida
+e a outra tirará um ponto. Espero que você escolha a certa todas as vezes, grande Guerreiro!`);
 
-if (playerDefense > opponentDefense) {
+//Ataque turno/rodadas / vida gigante 50
+//Gigante é lento ataca a cada tres rodadas - tirando 1 de vida
+
+
+let r = false
+let vida = 50
+let dano
+
+while (r == false);
+
+do{
+    dano = +prompt(`Digite o Dano do herói (5 a 10): `);
+    
+    if (dano < 5|| dano > 10 ){;
+        console.log(`Digite um valor entre 5 e 10.`);
+        r = false;
+    }
+    
+    else if (dano >= 5 && dano <= 10){;
+        r = true;
+    }}while (r == false);
+
+let calculo
+
+if(vida%dano == 0){
+    calculo = vida / dano
+}
+else if (vida%dano != 0 ){
+    calculo = (vida/dano) + 1
+}
+
+console.log(`A vida do Troll é de ${vida}Hp`)
+console.log(`O dano do Herói causa -${dano}Hp`)
+
+
+//----------------------------------------------------------------//
+
+console.log(`Bem vindo à Montanha da Perdição.
+
+Você avista inscrições na entrada.
+"Abandone toda a esperança, você que entra por esses portões."
+
+Você encontra uma figura raquítica, um senhor, que diz: Antes de mais nada você tem que responder 
+algumas perguntas (nunca se sabe, de repente você ganha algo)!`);
+console.log(`Você precisa responder ao seguinte questionário. Será que você conhece o inferno?!?!?
+Para todos os efeitos, irei lhe chamar do mesmo nome do último visitante, Dante.`);
+
+var p1r2= prompt('Quantos níveis tem nessa montanha?');
+var p2r2= prompt('Qual o crime mais grave desta região?');
+var p3r2= prompt('Quem sou eu?');
+
+console.log(`Você deve ter conhecido Caronte, o barqueiro. Sujeito grave, taciturno, "poucas ideias".
+`)
+
+
+
+- Dentro Vulcão/montanha
+* Labirinto - pasagem de tempo
+*
+* Luta chefão minotauro - vida - dinheiro/itens
+
+
+
+//----------------------------------------------------------------//
+
+
+- Reino Gelo
+* Velho Misterioso - Jogar cara ou coroa para ganhar uma rede de pesca
+* Pescar pela sobrevivencia - Se tiver a rede de pesca pega peixes que restauram de 60% a 80%
+se não tiver a rede de pescar ele pesca so um peixe que restaura até 20%
+* Luta chefão Dragão de gelo - vida - dinheiro/itens
+
+//Reino de Gelo
+// O personagem se depara com um velho misterioso que lhe oferece uma rede de pesca para
+// pegar peixes e se alimentar recuperando um pouco de vida/energia
+// Mas para conseguir rede o personagem deve jogar cara ou coroa com o velho
+// O jogo tem 3 rodas
+
+let vitoriaPersonagem = 0;
+let vitoriaVelho = 0;
+
+for (let i = 0; i < 3; i++) {
+  let escolhaVelho = 0;
+  let escolhaPersonagem = 0;
+  let faceMoeda = 0;
+
+  do {
+    console.log(`\nRodada ${i + 1}\n`);
+    escolhaPersonagem = parseInt(
+      prompt('Escolha uma opção: 1 - Cara | 2 - Coroa: ')
+    );
+    if (escolhaPersonagem == 1) {
+      escolhaVelho = 2;
+    } else if (escolhaPersonagem == 2) {
+      escolhaVelho = 1;
+    } else {
+      console.log('Opção inválida!! \n');
+    }
+    faceMoeda = Math.floor(2 * Math.random() + 1);
+  } while (escolhaPersonagem != 1 && escolhaPersonagem != 2);
+
+  if (escolhaPersonagem == faceMoeda) {
+    vitoriaPersonagem++;
+  } else {
+    vitoriaVelho++;
+  }
+
   console.log(
-    `\n[WIN] You carried yourself well. The positive attributes you have shown throughout this journey have earned you the title of Supreme Ruler. Congratulations.\n`
-  );
-} else if (playerDefense < opponentDefense) {
-  console.log(
-    `\n[LOSE] Your choices in life have led to your inevitable defeat.\n`
-  );
-} else if (playerDefense == opponentDefense) {
-  console.log(
-    `\n[DRAW] Neither you nor your opponent were able to convince the people that you are fit to rule... The search for a new leader continues.\n`
+    `Placar: Velho: ${vitoriaVelho} Personagem: ${vitoriaPersonagem}`
   );
 }
+
+if (vitoriaPersonagem > vitoriaVelho) {
+  console.log('\nParabens! \nVocê ganhou o duelo e recebeu uma rede de pesca!');
+} else {
+  console.log('\nQue pena! \nVocê perdeu o duelo!');
+}
+
+
+
+// Pescar pela sobrevivencia - Se tiver a rede de pesca pega peixes que restauram de 50% a 70%
+// da vida/energia. Se não tiver a rede de pescar ele usa outro item e pesca so um peixe que restaura até 20%
+
+// if(iten existe na sacola de itens){
+//     faz um random para recuperar de 50% a 70% da vida/energia
+// }else{
+//     faz um randon para recuperar até 20%
+// }
+
+
+// Luta chefão Dragão de gelo - vida - dinheiro/itens
+
+//Implementar aqui a mesma logica dos outros chefões
+*/
